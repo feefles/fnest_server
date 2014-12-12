53cf50376cd78e268009aa02938a5e465c8c4621
root <eyeung@seas.upenn.edu>
2014-12-12 02:56:18 +0000

    added mmap files

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 from fnest import fnest
+import os
 
-fnest.run(debug = True)
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 3933))
+	fnest.run(host='0.0.0.0', port=port)
